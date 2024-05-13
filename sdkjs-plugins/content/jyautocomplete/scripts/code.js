ba0f079755b3146eaf2480a7bc027daf8e4bdc7b
zhouxiaosen <254469749@qq.com>
2024-05-13 11:09:26 +0800

    feat: ✨根据输入提示中文

--- sdkjs-plugins/content/jyautocomplete/scripts/code.js
+++ sdkjs-plugins/content/jyautocomplete/scripts/code.js
@@ -68,7 +68,7 @@
 				window.Asc.plugin.currentText = window.Asc.plugin.currentText.substr(lastIndexSpace + 1);
 		}
 
-		if (window.Asc.plugin.currentText.length < 3)
+		if (window.Asc.plugin.currentText.length < 2)
 		{
 			window.Asc.plugin.getInputHelper().unShow();
 			return;
@@ -113,37 +113,72 @@
 	window.isAutoCompleteReady = false;
 	window.getAutoComplete = function(text)
 	{
-		
+		if (!g_dictionary)
+			return;
 		window.isAutoCompleteReady = true;
-
+		g_dictionary.sort();
 		var textFound = text.toLowerCase();
 
 		var start = 0;
-		// var end = g_dictionary.length - 1;
+		var end = g_dictionary.length - 1;
 		var index = 0;
 
-		
+		while (true)
+			{
+				var middle = (end + start) >> 1;
+	
+				if (middle == start || middle == end)
+				{
+					index = start;
+	
+					while (index != end)
+					{
+						if (g_dictionary[index] >= textFound)
+							break;
+						index++;
+					}
+	
+					break;
+				}
+	
+				var test = g_dictionary[middle];
+	
+				if (test == textFound)
+				{
+					index = middle;
+					break;
+				}
+	
+				if (test < textFound)
+				{
+					start = middle;				
+				}
+				else
+				{
+					end = middle;
+				}
+			}
 
 		var ret = [];
-		// end = g_dictionary.length;
-		// while (index < end)
-		// {
-		// 	var testRec = g_dictionary[index++];
-		// 	if (testRec.indexOf(textFound) != 0)
-		// 		break;
+		end = g_dictionary.length;
+		while (index < end)
+		{
+			var testRec = g_dictionary[index++];
+			if (testRec.indexOf(textFound) != 0)
+				break;
 
-		// 	ret.push(text + testRec.substr(textFound.length));
-		// 	index++;
-		// }
-		$.ajax({
-			url:'https://gw-test.jianyiqifu.com/jy-comp/pws/cmsctr/search/do_cms_help_type_search',
-			method:'POST',
-			data: {typeName:text},
-			success:function(data){
-				ret.push(text + data.substr(textFound.length));
-				// window.isAutoCompleteReady = true;
-			}
-		})
+			ret.push(text + testRec.substr(textFound.length));
+			index++;
+		}
+		// $.ajax({
+		// 	url:'https://gw-test.jianyiqifu.com/jy-comp/pws/cmsctr/search/do_cms_help_type_search',
+		// 	method:'POST',
+		// 	data: {typeName:text},
+		// 	success:function(data){
+		// 		ret.push(text + data.substr(textFound.length));
+		// 		// window.isAutoCompleteReady = true;
+		// 	}
+		// })
 
 		return ret;
 	};
