bcc9122f7b5ca7f6afd8e2735c383ee7d1bab655
zhouxiaosen <254469749@qq.com>
2024-05-14 16:28:00 +0800

    feat: ✨修改插件地址

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "https://ibbking.github.io/onlyoffice.github.io/sdkjs-plugins/content/jyInsertInput/";
+	var URL_TO_PLUGIN = "https://ibbking.github.io/onlyoffice.github.io/sdkjs-plugins/content/jyAddAnnotations/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
