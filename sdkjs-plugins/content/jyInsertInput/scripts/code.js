03a34bfb13cda3e19fceeaa89d8a08c32de708f9
zhouxiaosen <254469749@qq.com>
2024-05-14 11:11:26 +0800

    feat: 添加表单

--- sdkjs-plugins/content/jyInsertInput/scripts/code.js
+++ sdkjs-plugins/content/jyInsertInput/scripts/code.js
@@ -15,34 +15,47 @@
  * limitations under the License.
  *
  */
-(function(window, undefined){
+(function (window, undefined) {
 	var flagInit = false;
 	var fBtnGetAll = false;
 	var fClickLabel = false;
-	var fClickBtnCur =  false;
+	var fClickBtnCur = false;
+	var isInit = false;
+	window.Add = function (value,len = 10) {
+		if (!isInit)
+			return;
 
-    window.Asc.plugin.init = function(text)
-    {
+		// serialize command as text
+		var oDocument = Api.GetDocument();
+		var oTextForm = Api.CreateTextForm({ "key": "Personal information", "tip": "Enter your first name", "required": true, "placeholder": "First name", "comb": true, "maxCharacters": len, "cellWidth": 3, "multiLine": false, "autoFit": true });
+		var oParagraph = oDocument.GetElement(0);
+		oParagraph.AddElement(oTextForm);
+		window.Asc.plugin.executeMethod("PasteText", [value]);
+		
+	};
+
+	window.Asc.plugin.init = function (text) {
+		isInit = true
 		//event "init" for plugin
-		document.getElementById("divS").innerHTML = text.replace(/\n/g,"<br>");
+		document.getElementById("divS").innerHTML = text.replace(/\n/g, "<br>");
 
-		document.getElementById("buttonIDPaste").onclick = function() {
+		document.getElementById("buttonIDPaste").onclick = function () {
 			//method for paste text into document
-			window.Asc.plugin.executeMethod("PasteText", ["海康威视"]);	
+			window.Asc.plugin.executeMethod("PasteText", ["海康威视"]);
 
 		};
 
-		
 
-		document.getElementById("buttonIDGetAll").onclick = function() {
+
+		document.getElementById("buttonIDGetAll").onclick = function () {
 			//method for get all content controls
 			window.Asc.plugin.executeMethod("GetAllContentControls");
-			fBtnGetAll = true;					
+			fBtnGetAll = true;
 
 		};
 
-		document.getElementById("buttonIDShowCurrent").onclick = function() {
-			
+		document.getElementById("buttonIDShowCurrent").onclick = function () {
+
 			fClickBtnCur = true;
 			//menthod for get current content control (where is the cursor located)
 			window.Asc.plugin.executeMethod("GetCurrentContentControl");
@@ -56,71 +69,67 @@
 			// document.getElementById("buttonIDGetAll").click();
 		}
 	};
-	
+
 	addLabel = (returnValue, element) => {
 		$(element).append(
-			$('<label>',{
-				id : returnValue.InternalId,
-				for : element,
-				class : 'label-info',
-				text : returnValue.InternalId + "	" + (returnValue.Id || 'null'),
-				on : {
-					click: function(){
+			$('<label>', {
+				id: returnValue.InternalId,
+				for: element,
+				class: 'label-info',
+				text: returnValue.InternalId + "	" + (returnValue.Id || 'null'),
+				on: {
+					click: function () {
 						fClickLabel = true;
 						$('.label-selected').removeClass('label-selected');
 						$(this).addClass('label-selected');
 						if (element === "#divG") {
 							//method for select content control by id
-							window.Asc.plugin.executeMethod("SelectContentControl",[this.id]);
+							window.Asc.plugin.executeMethod("SelectContentControl", [this.id]);
 						} else {
 							//method for move cursor to content control with specified id
-							window.Asc.plugin.executeMethod("MoveCursorToContentControl",[this.id, true]);
+							window.Asc.plugin.executeMethod("MoveCursorToContentControl", [this.id, true]);
 						}
 					},
-					mouseover: function(){
+					mouseover: function () {
 						$(this).addClass('label-hovered');
 					},
-					mouseout: function(){
+					mouseout: function () {
 						$(this).removeClass('label-hovered');
 					}
 				}
 			})
 		);
 	};
-	
-    window.Asc.plugin.button = function()
-    {
-		this.executeCommand("close", "");
-    };
 
-	window.Asc.plugin.onMethodReturn = function(returnValue)
-	{
+	window.Asc.plugin.button = function () {
+		this.executeCommand("close", "");
+	};
+
+	window.Asc.plugin.onMethodReturn = function (returnValue) {
 		//evend return for completed methods
 		var _plugin = window.Asc.plugin;
-		if (_plugin.info.methodName == "GetAllContentControls")
-		{
+		if (_plugin.info.methodName == "GetAllContentControls") {
 			if (fBtnGetAll) {
 				document.getElementById("divP").innerHTML = "";
 				fBtnGetAll = false;
-				for (var i = 0; i < returnValue.length; i++) {	
+				for (var i = 0; i < returnValue.length; i++) {
 					addLabel(returnValue[i], "#divP");
 				}
 			} else {
 				document.getElementById("divG").innerHTML = "";
-				for (var i = 0; i < returnValue.length; i++) {	
+				for (var i = 0; i < returnValue.length; i++) {
 					addLabel(returnValue[i], "#divG");
 				}
 			}
-			
+
 
 		} else if (_plugin.info.methodName == "GetCurrentContentControl") {
 			if (fClickBtnCur) {
 				//method for select content control by id
-				window.Asc.plugin.executeMethod("SelectContentControl",[returnValue]);
+				window.Asc.plugin.executeMethod("SelectContentControl", [returnValue]);
 				fClickBtnCur = false;
 			} else if (!($('.label-selected').length && $('.label-selected')[0].id === returnValue) && returnValue) {
-				if (document.getElementById(returnValue))
-				{
+				if (document.getElementById(returnValue)) {
 					$('.label-selected').removeClass('label-selected');
 					$('#divG #' + returnValue).addClass('label-selected');
 					$('#divP #' + returnValue).addClass('label-selected');
@@ -128,7 +137,7 @@
 
 				} else {
 					$('.label-selected').removeClass('label-selected');
-					addLabel({InternalId: returnValue},"#divG");
+					addLabel({ InternalId: returnValue }, "#divG");
 					$('#' + returnValue).addClass('label-selected');
 				}
 			} else if (!returnValue) {
@@ -136,9 +145,8 @@
 			}
 		}
 	};
-	
-	window.Asc.plugin.event_onTargetPositionChanged = function()
-	{
+
+	window.Asc.plugin.event_onTargetPositionChanged = function () {
 		//event change cursor position
 		//all events are specified in the config file in the "events" field
 		if (!fClickLabel) {
